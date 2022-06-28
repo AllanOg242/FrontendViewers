98104da5fa8ee8fb2850b238c9c177ccd99ee866
AllanOg242 <ogounchiallan23@gmail.com>
2022-06-28 11:17:37 +0200

    ressources

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  html,
+  render,
+  useReducer,
+  useMemo,
+  useEffect,
+} from './lib/htm-preact.js'
+import debounce from './utils/debounce.js'
+import { reducer, initialState } from './state.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Article } from './routes/article.js'
+import { Category } from './routes/category.js'
+
+import { About } from './routes/about.js'
+import { Contact } from './routes/contact.js'
+import { Home } from './routes/home.js'
+import { getActiveItemId, getPageName } from './utils/path.js'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const pageName = state?.pageName
+  const CurrentPage = useMemo(() => {
+      let Page = Home
+      if (pageName === 'about') {
+          Page = About
+      } else if (pageName === 'contact') {
+          Page = Contact
+      } else if (pageName === 'categories') {
+          Page = Category
+      } else if (pageName === 'articles') {
+          Page = Article
+      }
+      return Page
+  }, [pageName])
+
+  useEffect(() => {
+      const showState = () => {
+          debouncedConsole(state)
+      }
+      window.addEventListener('showState', showState)
+      return function cleanup() {
+          window.removeEventListener('showState', showState)
+      }
+  }, [state])
+
+  useEffect(() => {
+      const updatePath = () => {
+          dispatch({
+              type: 'URI_CHANGE',
+              pageName: getPageName(),
+              activeItemId: getActiveItemId(),
+          })
+      }
+      window.addEventListener('popstate', updatePath)
+      return function cleanup() {
+          window.removeEventListener('popstate', updatePath)
+      }
+  }, [dispatch])
+
+  return html`<${CurrentPage} state=${state} dispatch=${dispatch} />`
 }
 
-export default App;
+render(html`<${App} />`, document.getElementById('app-mount'))
+
+Object.defineProperty(window, 'state', {
+  async get() {
+      dispatchEvent(new CustomEvent('showState'))
+  },
+  configurable: true,
+  enumerable: true,
+})
+const debouncedConsole = debounce(console.log, 1000)
